17aff27820a6c8b539a7f8cad7911485565309d3
ABPW10 <106003567+ABPW10@users.noreply.github.com>
2026-08-22 00:14:53 +0800

    Delete public_html directory

--- public_html/weather/api/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { defaultRoute } from "./default";
-import { locationRoute } from "./location";
-import { weatherRouter } from "./weather";
-
-export const routes = express.Router();
-
-routes.use(defaultRoute, locationRoute, weatherRouter);
--- public_html/weather/frontend/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template lang="html">
-  <div>
-    <h1>Hello World</h1>
-  </div>
-</template>
